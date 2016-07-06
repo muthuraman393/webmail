994c9a5ddf002661768b238a370cd1e8849d54e9
muthuraman393 <muthuraman393@gmail.com>
2016-07-06 17:54:24 +0530

    7-6

--- app/authCtrl.js
+++ app/authCtrl.js
@@ -1,4 +1,4 @@
-var navCtrl = app.controller('navCtrl', ['Entry' ,function($scope, $rootScope, $routeParams, $location, $filter,$http,Entry) {
+var navCtrl = app.controller('navCtrl', ['$scope', '$rootScope', '$routeParams', '$location', '$filter','$http','Entry','$timeout' ,function($scope, $rootScope, $routeParams, $location, $filter,$http,Entry,$timeout) {
     $scope.homeitems = [{
         nameid: 'ford_1',
         src: 'img/puzzle/casa3x2/fiesta/Ford_Original.png',
@@ -20,29 +20,32 @@ var navCtrl = app.controller('navCtrl', ['Entry' ,function($scope, $rootScope, $
     $scope.query = {}
     $scope.queryBy = '$'
 
-
-
     /*$http.get("assets/api/data.json")
-     $http.get("https://raw.githubusercontent.com/muthuraman393/webmail/c37e178dd978af03b09fddfc35b388a79d0e5cc6/data.json")
     .then(function(response) {
         $scope.list2 = response.data;
     });*/
 
-    var entries = Entry.query(function() {
-    console.log(entries);
-    $scope.list2 = entries;
-     }); //query() returns all the entries
+    var entries = Entry.query().$promise.then(function(value) {
+    //$timeout(function () {
+    //alert('asd');
+    console.log(value);
+    $scope.list2 = value;
 
-    $scope.list2 = response.data;
+    //}, 2000);
+    }); //query() returns all the entries
+
+    
+
+    
 
     angular.forEach($scope.list2, function(item) {
         item.Selected = false;
     });
-
+    $scope.boolChangeClass = false;
     $scope.visiblecompose = true; //to true
     $scope.visiblepencil = false;
     $scope.visiblecross = true;
-    $scope.visibleinnermail = true;
+    //$scope.visibleinnerreadmail = [];
     $scope.visibleinnermailopensend = true;
     $scope.tickboxunselected = false;
     $scope.toreplyclass = false;
@@ -68,20 +71,30 @@ var navCtrl = app.controller('navCtrl', ['Entry' ,function($scope, $rootScope, $
         //$scope.visiblecompose = true;
     };
 
-    $scope.innermailopen = function() {
-        $scope.visibleinnermail = false;
 
-    };
-    $scope.innermailclose = function() {
-        $scope.visibleinnermail = true;
+    $scope.innermailopen = function(index) {
+       //alert(index+'open');
+       console.log($scope.list2[index]);
+       $scope.closeallinnermails();
+       $scope.list2[index].visibleinnerreadmail = 'open'; 
 
+        
     };
+
+    $scope.closeallinnermails = function(index) {
+        angular.forEach($scope.list2, function(item) {
+        item.visibleinnerreadmail = 'close';
+        });
+    };
+
+
     $scope.stateChanged = function(tickid, qId) {
-        if ($scope.list2[tickid - 1].Selected == false) {
-            //alert('test'+$scope.list2[tickid-1].Selected+'_innermail'+$scope.visibleinnermail);
+         var falsecount = 0;
+        if ($scope.list2[tickid].Selected == false) {
+            //alert('test'+$scope.list2[tickid].Selected+'_innermail'+$scope.visibleinnermail);
             $scope.visibleinnermailopensend = true;
             $scope.toreplyclass = false;
-            var falsecount = 0;
+           
 
             angular.forEach($scope.list2, function(item) { /*to check uncheck master*/
                 if (item.Selected == true) {
@@ -98,10 +111,10 @@ var navCtrl = app.controller('navCtrl', ['Entry' ,function($scope, $rootScope, $
             });
 
         }
-        if ($scope.list2[tickid - 1].Selected == true) {
+        if ($scope.list2[tickid].Selected == true) {
             $scope.visibleinnermailopensend = false;
             $scope.toreplyclass = true;
-
+             
             angular.forEach($scope.list2, function(item) { /*to check uncheck master*/
                 if (item.Selected == true) {
                     $scope.tickboxunselected = true;
@@ -117,12 +130,22 @@ var navCtrl = app.controller('navCtrl', ['Entry' ,function($scope, $rootScope, $
             });
 
         }
+
+        if(falsecount<=0)
+		$scope.tickboxunselected=false;
+
+        console.log($scope.tickboxunselected,falsecount);
+        callapplymanual($scope);                  
+
+
     };
 
+    var callapplymanual = function($scope){
+             
+    }
 
 
-
-    /* 		$scope.innermailopensend = function() {
+    /* $scope.innermailopensend = function() {
             $scope.visibleinnermailopensend = $scope.visibleinnermailopensend?false:true;
             }; */
 
@@ -130,10 +153,10 @@ var navCtrl = app.controller('navCtrl', ['Entry' ,function($scope, $rootScope, $
         $scope.tickboxunselected = false;
         if ($scope.selectedAll) {
             //alert('if'+$scope.selectedAll);
-            $scope.selectedAll = false;
+            $scope.selectedAll = true
         } else {
             //alert('else'+$scope.selectedAll);
-            $scope.selectedAll = true;
+            $scope.selectedAll = false;
         }
         angular.forEach($scope.list2, function(item) {
             item.Selected = $scope.selectedAll;
